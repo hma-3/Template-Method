cb27121e8e061f4a7420f8c80ec73b2fde3e6d68
hma-3 <maria.gula0307@gmail.com>
2023-12-17 12:06:51 +0200

    Add project files.

---
+++ Template Method.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class AbstractClass {
+    virtual void step1() {
+        cout << "Abstract Step 1" << endl;
+    }
+
+    virtual void step2() = 0;
+
+    virtual void step3() {
+        cout << "Abstract Step 3" << endl;
+    }
+public:
+    void templateMethod() {
+        step1();
+        step2();
+        step3();
+    }
+};
+
+class ConcreteClass : public AbstractClass {
+    void step2() override {
+        cout << "Concrete Step 2" << endl;
+    }
+};
+
+int main() {
+    ConcreteClass concreteObject;
+    concreteObject.templateMethod();
+
+    return 0;
+}
